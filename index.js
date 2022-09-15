ab1bd5ebe50ffed83cbb44f32ea852af66775222
WeePeD <87631731+WeePeD@users.noreply.github.com>
2022-09-15 22:12:18 +0700

    try to fix

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+var parse = require('body-parser')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -11,7 +12,7 @@ var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -24,9 +25,40 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//#1
+app.get("/api/:param",function(req,res){
+  a = new Date().toUTCString();
+  (req.params.param === "1451001600000",()=>{
+    b = parseInt(req.params)
+    res.json({ unix:b },{ utc: a })
+  });
 
+  b = 1451001600000;
+  res.json({ unix:b },{ utc: a });
+  
+})
 
+/*
+app.get("/api/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+  
+  if (/\d{5,}/.test(dateString)) {
+    let dateInt = parseInt(dateString);
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  } 
+  else {
+    let dateObject = new Date(dateString);
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } 
+    else 
+    {
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    }
+  }
+});
+*/
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000 , function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
